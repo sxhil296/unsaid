faf1b614b20fab4b6e2942d6bc1c5a4e35ff7825
sahil <leviack629@gmail.com>
2025-07-10 00:40:49 +0530

    add msg number

--- src/app/archives/page.tsx
+++ src/app/archives/page.tsx
@@ -47,7 +47,7 @@ export default function ArchivesPage() {
           Archives
         </h1>
         <p className="mt-4 text-lg text-center text-gray-600">
-          <strong>2345</strong> messages have been shared on{" "}
+          <strong className="text-main">{messages.length}</strong> messages have been shared on{" "}
           <em className="font-bold">Unsaid</em> so far — each one a window into
           someone&apos;s unspoken thoughts. Here, you can explore the heartfelt
           words people chose not to send, but still wanted to be heard.
